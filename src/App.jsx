5ff3bb7603993a20c8350ba9e88ecf3ace4fe9a7
Rodrigo Solis <rodrigo.solis.g@gmail.com>
2023-10-22 16:01:11 -0300

    ajuste los componentes en un grid para que tengan mejor visualizacion

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,16 @@ import Jugador from "/src/components/Jugador.jsx";
 import Button from "/src/components/Button.jsx"
 
 const App = () => {
+
+  let guionAnimado = document.querySelector('.underScore')
+
+  // console.log(opacidadGuion)
+  
+/*   setInterval(() => {
+      opacidadGuion === "100%" ? guionAnimado.style.opacity = "100%" : guionAnimado.style.opacity = "0%"
+  }, 1000); */
+
+
   return (
     <>
       {/* este es el titulo del juego */}
@@ -11,11 +21,14 @@ const App = () => {
         <h1>React</h1>
       </div>
       
-        
+      <div className="mainContainer">
         <h6>Ingresa el nombre de ambos jugadores para comenzar.</h6>
         <Jugador numeroDeJugador={1} nombreJugador="Jugador 1" iconoDeJugador={true} conteoDePartidas={false}/>
         <Jugador numeroDeJugador={2} nombreJugador="Jugador 2" iconoDeJugador={false} conteoDePartidas={false}/>
         <Button textoDelBoton="Comenzar" tipoDeBotton="Primario"/>
+      </div>
+
+      <footer> <p className='smallText'>Design & code by : Rodd Solis</p><p className='underScore'>_</p></footer>
   
     </>
   );
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,9 @@ const Button = ({textoDelBoton}) => {
 
   return (
     <>
+      <div className="actionsContainer">
       <button className="btn-primary">{textoDelBoton}</button> {/* para activar el boton hay que eliminar la clase btn-disabled para inabilitar agregar nuevamente la clase*/}
+      </div>
     </>
   )
 }
